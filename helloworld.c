900c305b0190dbbf5b1da001b36ece4fd624fc59
khal-at-github <123749356+khal-at-github@users.noreply.github.com>
2023-03-16 22:17:21 -0400

    Add files via upload

---
+++ helloworld.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <windows.h>
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+    printf("This is my first app running on the Windows operating system made using C programming language\n");
+    system("pause"); // Wait for 20 seconds
+    return 0;
+}
